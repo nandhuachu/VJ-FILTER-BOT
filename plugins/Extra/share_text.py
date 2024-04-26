c15968d6e8d8a62ae10cbcd3cd0c63d84014f76a
Venom <67759177+nandhuachu@users.noreply.github.com>
2024-04-26 16:44:02 +0530

    Update share_text.py

--- plugins/Extra/share_text.py
+++ plugins/Extra/share_text.py
@@ -21,7 +21,7 @@ async def share_text(client, message):
         await message.reply_text(
             text=f"**Notice:**\n\n1. Reply Any Messages.\n2. No Media Support\n\n**Any Question Join Support Chat**",                
             reply_to_message_id=reply_id,               
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Updates channel", url=f"https://t.me/tvseriezzz_update")]])
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Updates channel", url=f"https://t.me/tvseriezzz_updates")]])
             )                                                   
         return
     await message.reply_text(
