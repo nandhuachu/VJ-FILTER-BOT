de303c4f9165ea3b37b4ab02dd9db5278dae78d1
Venom <67759177+nandhuachu@users.noreply.github.com>
2024-04-30 23:15:33 +0530

    Update tts.py

--- plugins/Extra/tts.py
+++ plugins/Extra/tts.py
@@ -14,7 +14,7 @@ from pyrogram.types import Message
 
 def convert(text):
     audio = BytesIO()
-    i = Translator().translate(text, dest="en")
+    i = Translator().translate(text, dest="ml")
     lang = i.src
     tts = gTTS(text, lang=lang)
     audio.name = lang + ".mp3"
