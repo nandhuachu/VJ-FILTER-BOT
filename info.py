cb8515a663b737bf16b123f3b60b2d930bef3587
Venom <67759177+nandhuachu@users.noreply.github.com>
2024-05-02 08:22:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -127,7 +127,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://cheerful-bree-nandhuachu.koyeb.app/")
+URL = environ.get("URL", "https://straightforward-kellen-nandhuachu.koyeb.app/")
 
 
 # Rename Info ; If True Then Bot Rename File Else Not
