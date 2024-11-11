5a3d3fddf0c384333f7ca318d591e1477b5fd9a7
Venom <67759177+nandhuachu@users.noreply.github.com>
2024-11-11 20:37:09 +0530

    Update info.py

--- info.py
+++ info.py
@@ -50,7 +50,20 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
+MULTIPLE_DATABASE = bool(environ.get('MULTIPLE_DATABASE', False)) # Set True or False
+
+DATABASE_URI = environ.get('DATABASE_URI', "") # IF Multiple Database Is False Then Fill Only This Database Url.
+if MULTIPLE_DATABASE == False:
+    USER_DB_URI = DATABASE_URI
+    OTHER_DB_URI = DATABASE_URI
+    FILE_DB_URI = DATABASE_URI
+    SEC_FILE_DB_URI = DATABASE_URI
+else:
+    USER_DB_URI = DATABASE_URI # This Db is for User Data Store
+    OTHER_DB_URI = environ.get('OTHER_DB_URI', "mongodb+srv://dbase3232:dbase3232@cluster0.wzpjny4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # This Db Is For Other Data Store
+    FILE_DB_URI = environ.get('FILE_DB_URI', "mongodb+srv://venom:venom@cluster0.54isybk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # This Db Is For File Data Store
+    SEC_FILE_DB_URI = environ.get('SEC_FILE_DB_URI', "mongodb+srv://venom:venom@cluster0.rshk2ut.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # This Db is for File Data Store When First Db Is Going To Be Full.
+
 DATABASE_NAME = environ.get('DATABASE_NAME', "MrCVENOMbot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
