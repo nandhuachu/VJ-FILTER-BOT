586f7ead2078dda2e44176d1af60ca17a566e492
Venom <67759177+nandhuachu@users.noreply.github.com>
2024-11-11 21:39:51 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -108,7 +108,7 @@ async def start(client, message):
         try:
             invite_link = await client.create_chat_invite_link(int(AUTH_CHANNEL))
         except ChatAdminRequired:
-            logger.error("Make sure Bot is admin in Forcesub channel")
+            await message.reply_text("Make sure Bot is admin in Forcesub channel")
             return
         btn = [
             [
