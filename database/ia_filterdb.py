6364813733a743d909abe98feb71125fb614d115
Venom <67759177+nandhuachu@users.noreply.github.com>
2024-11-18 18:03:55 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -11,37 +11,19 @@ from pymongo.errors import DuplicateKeyError
 from umongo import Instance, Document, fields
 from motor.motor_asyncio import AsyncIOMotorClient
 from marshmallow.exceptions import ValidationError
-from info import FILE_DB_URI, SEC_FILE_DB_URI, DATABASE_NAME, COLLECTION_NAME, USE_CAPTION_FILTER, MAX_B_TN
+from info import DATABASE_URI, DATABASE_NAME, COLLECTION_NAME, USE_CAPTION_FILTER, MAX_B_TN
 from utils import get_settings, save_group_settings
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-client = AsyncIOMotorClient(FILE_DB_URI)
+client = AsyncIOMotorClient(DATABASE_URI)
 db = client[DATABASE_NAME]
 instance = Instance.from_db(db)
 
 @instance.register
 class Media(Document):
-    file_id = fields.StrField(attribute='_id')
-    file_ref = fields.StrField(allow_none=True)
-    file_name = fields.StrField(required=True)
-    file_size = fields.IntField(required=True)
-    file_type = fields.StrField(allow_none=True)
-    mime_type = fields.StrField(allow_none=True)
-    caption = fields.StrField(allow_none=True)
-
-    class Meta:
-        indexes = ('$file_name', )
-        collection_name = COLLECTION_NAME
-
-sec_client = AsyncIOMotorClient(SEC_FILE_DB_URI)
-sec_db = sec_client[DATABASE_NAME]
-sec_instance = Instance.from_db(sec_db)
-
-@sec_instance.register
-class Media2(Document):
     file_id = fields.StrField(attribute='_id')
     file_ref = fields.StrField(allow_none=True)
     file_name = fields.StrField(required=True)
@@ -61,13 +43,8 @@ async def save_file(media):
     # TODO: Find better way to get same file_id for same media to avoid duplicates
     file_id, file_ref = unpack_new_file_id(media.file_id)
     file_name = re.sub(r"(_|\-|\.|\+)", " ", str(media.file_name))
-    data_size = (await db.command("dbstats"))['dataSize']
-    if data_size > 503316480:
-        VJMedia = Media2
-    else:
-        VJMedia = Media
     try:
-        file = VJMedia(
+        file = Media(
             file_id=file_id,
             file_ref=file_ref,
             file_name=file_name,
@@ -80,25 +57,19 @@ async def save_file(media):
         logger.exception('Error occurred while saving file in database')
         return False, 2
     else:
-        if VJMedia == Media2:
-            check = Media.find(file)
-            if check:
-                print(f"{file_name} is already saved.")
-                return False, 0
-
         try:
             await file.commit()
         except DuplicateKeyError:      
-            print(f"{file_name} is already saved.")
+            logger.warning(
+                f'{getattr(media, "file_name", "NO_FILE")} is already saved in database'
+            )
+
             return False, 0
         else:
-            print(f"{file_name} is saved to database.")
+            logger.info(f'{getattr(media, "file_name", "NO_FILE")} is saved to database')
             return True, 1
 
-async def get_file_db_size():
-    data_size = (await db.command("dbstats"))['dataSize']
-    data_size2 = (await sec_db.command("dbstats"))['dataSize']
-    return data_size, data_size2
+
 
 async def get_search_results(chat_id, query, file_type=None, max_results=10, offset=0, filter=False):
     """For given query return (results, next_offset)"""
@@ -141,26 +112,19 @@ async def get_search_results(chat_id, query, file_type=None, max_results=10, off
     if file_type:
         filter['file_type'] = file_type
 
-    result1 = await Media.count_documents(filter)
-    result2 = await Media2.count_documents(filter)
-    total_results = result1 + result2
+    total_results = await Media.count_documents(filter)
     next_offset = offset + max_results
 
     if next_offset > total_results:
         next_offset = ''
 
-    cursor1 = Media.find(filter)
-    cursor2 = Media2.find(filter)
+    cursor = Media.find(filter)
     # Sort by recent
-    cursor1.sort('$natural', -1)
-    cursor2.sort('$natural', -1)
+    cursor.sort('$natural', -1)
     # Slice files according to offset and max results
-    cursor1.skip(offset).limit(max_results)
-    cursor2.skip(offset).limit(max_results)
+    cursor.skip(offset).limit(max_results)
     # Get list of files
-    files1 = await cursor1.to_list(length=max_results)
-    files2 = await cursor2.to_list(length=max_results)
-    files = files1 + files2
+    files = await cursor.to_list(length=max_results)
 
     return files, next_offset, total_results
 
@@ -191,19 +155,13 @@ async def get_bad_files(query, file_type=None, filter=False):
     if file_type:
         filter['file_type'] = file_type
 
-    result1 = await Media.count_documents(filter)
-    result2 = await Media2.count_documents(filter)
-    total_results = result1 + result2
+    total_results = await Media.count_documents(filter)
 
-    cursor1 = Media.find(filter)
-    cursor2 = Media2.find(filter)
+    cursor = Media.find(filter)
     # Sort by recent
-    cursor1.sort('$natural', -1)
-    cursor2.sort('$natural', -1)
+    cursor.sort('$natural', -1)
     # Get list of files
-    files1 = await cursor1.to_list(length=max_results)
-    files2 = await cursor2.to_list(length=max_results)
-    files = files1 + files2
+    files = await cursor.to_list(length=total_results)
 
     return files, total_results
 
@@ -211,9 +169,6 @@ async def get_file_details(query):
     filter = {'file_id': query}
     cursor = Media.find(filter)
     filedetails = await cursor.to_list(length=1)
-    if not filedetails:
-        cursor1 = Media2.find(filter)
-        filedetails = await cursor1.to_list(length=1)
     return filedetails
 
 
